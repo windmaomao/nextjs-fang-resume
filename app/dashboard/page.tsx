7c3fa44f74a616360dfd992252b6c3365438e7a6
Fang Jin <windmaomao@gmail.com>
2025-08-08 13:36:34 -0400

    apply suspense to invoices section

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -3,14 +3,14 @@ import { Card } from '@/app/ui/dashboard/cards';
 import RevenueChart from '@/app/ui/dashboard/revenue-chart';
 import LatestInvoices from '@/app/ui/dashboard/latest-invoices';
 import { lusitana } from '@/app/ui/fonts';
-import { fetchLatestInvoices, fetchCardData } from '@/app/lib/data';
-import { RevenueChartSkeleton } from '@/app/ui/skeletons';
+import { fetchCardData } from '@/app/lib/data';
+import {
+  RevenueChartSkeleton,
+  LatestInvoicesSkeleton,
+} from '@/app/ui/skeletons';
 
 export default async function Page() {
-  const [latestInvoices, cardData] = await Promise.all([
-    fetchLatestInvoices(),
-    fetchCardData(),
-  ]);
+  const [cardData] = await Promise.all([fetchCardData()]);
   const {
     totalPaidInvoices,
     totalPendingInvoices,
@@ -37,7 +37,9 @@ export default async function Page() {
         <Suspense fallback={<RevenueChartSkeleton />}>
           <RevenueChart />
         </Suspense>
-        <LatestInvoices latestInvoices={latestInvoices} />
+        <Suspense fallback={<LatestInvoicesSkeleton />}>
+          <LatestInvoices />
+        </Suspense>
       </div>
     </main>
   );
