713f50df9ba08c68df65a78b177a7f465bf32928
Fang Jin <windmaomao@gmail.com>
2025-08-10 21:04:42 -0400

    relocate hero and logo of resume

--- app/resume/layout.tsx
+++ app/resume/layout.tsx
@@ -6,10 +6,8 @@ export default async function Layout({
   children: React.ReactNode;
 }) {
   return (
-    <div className='flex h-screen flex-col md:flex-row md:overflow-hidden'>
-      <div className='flex-grow p-6 md:overflow-y-auto md:p-12'>
-        <div className={lemonade.className}>{children}</div>
-      </div>
+    <div className='flex h-screen flex-col items-center'>
+      <div className={lemonade.className}>{children}</div>
     </div>
   );
 }
--- app/resume/page.tsx
+++ app/resume/page.tsx
@@ -1,27 +1,15 @@
 import Image from 'next/image';
 import { lusitana } from '@/app/ui/fonts';
 import Logo from './logo';
+import Hero from './hero';
 
 export default async function Page() {
   return (
-    <div>
-      <Logo />
-      <div className='my-4 flex flex-col sm:flex-row justify-around items-center gap-8'>
-        <Image
-          src='/resume/hero-desktop.png'
-          width={690}
-          height={459}
-          className='w-full sm:w-3/5'
-          alt='Screenshots of the dashboard project showing desktop version'
-        />
-        <div className='w-full sm:w-2/5 flex flex-col gap-8'>
-          <div className='text-4xl/10 font-bold'>Build app with Confidence</div>
-          <div className={`${lusitana.className} text-md`}>
-            My name is Fang, 90% UI engineer and 10% UX designer. I build a site
-            with precision and confidence. Checkout my case studies below.
-          </div>
-        </div>
+    <>
+      <div className='max-w-[1024px] p-6'>
+        <Logo />
       </div>
-    </div>
+      <Hero />
+    </>
   );
 }
