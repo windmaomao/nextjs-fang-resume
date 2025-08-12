0ba93cca3b8eae7a239bcdf0e43c502e2cd7840c
Fang Jin <windmaomao@gmail.com>
2025-08-12 10:18:51 -0400

    revert to use default auth

--- middleware.ts
+++ middleware.ts
@@ -1,27 +1,7 @@
-import { NextRequest, NextResponse } from 'next/server';
+import NextAuth from 'next-auth';
+import { authConfig } from './auth.config';
 
-// 1. Specify protected and public routes
-const protectedRoutes = ['/dashboard'];
-const publicRoutes = ['/login', '/signup', '/', '/resume'];
-
-export default async function middleware(req: NextRequest) {
-  // 2. Check if the current route is protected or public
-  const path = req.nextUrl.pathname;
-  const isProtectedRoute = protectedRoutes.includes(path);
-  const isPublicRoute = publicRoutes.includes(path);
-
-  // 4. Redirect to /login if the user is not authenticated
-  if (isProtectedRoute) {
-    return NextResponse.redirect(new URL('/login', req.nextUrl));
-  }
-
-  // 5. Redirect to /dashboard if the user is authenticated
-  if (isPublicRoute && !req.nextUrl.pathname.startsWith('/dashboard')) {
-    return NextResponse.redirect(new URL('/dashboard', req.nextUrl));
-  }
-
-  return NextResponse.next();
-}
+export default NextAuth(authConfig).auth;
 
 export const config = {
   // https://nextjs.org/docs/app/building-your-application/routing/middleware#matcher
