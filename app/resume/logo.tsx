d15a859401869ec762b2ef9296ba4edc48460420
Fang Jin <windmaomao@gmail.com>
2025-08-16 10:13:13 -0400

    use native motion react integration

--- app/resume/logo.tsx
+++ app/resume/logo.tsx
@@ -1,15 +1,15 @@
-import Motion from './motion';
+import * as motion from 'motion/react-client';
 
 export default async function Logo() {
   return (
     <div className='text-xl pl-2 text-sky-800 flex gap-[8px]'>
-      <Motion
+      <motion.div
         className='text-violet-500'
         animate={{ x: 5 }}
         transition={{ delay: 0.1, duration: 0.8 }}
       >
         Q
-      </Motion>
+      </motion.div>
       <span>plot</span>
     </div>
   );
