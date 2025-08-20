c5fd45216aeddd40e97b1877e9ae52632cf7e629
Fang Jin <windmaomao@gmail.com>
2025-08-20 11:59:02 -0400

    stick my name in resume logo section

--- app/resume/components/logo.tsx
+++ app/resume/components/logo.tsx
@@ -1,15 +1,19 @@
 import * as motion from 'motion/react-client';
+import { FiGitBranch } from 'react-icons/fi';
 
 export default async function Logo() {
   return (
     <motion.div
-      className='text-xl pl-2 text-sky-800 flex'
+      className='text-xl pl-2 pb-2 text-sky-800 flex items-center gap-2'
       initial={{ y: -10, opacity: 0 }}
       whileInView={{ y: 0, opacity: 1 }}
       transition={{ delay: 0.8, duration: 0.6 }}
     >
-      <span className='text-violet-500'>Q</span>
-      <span>plot</span>
+      <FiGitBranch />
+      <div className='flex gap-3 items-end'>
+        <span className=''>Fang Jin</span>
+        <span className='font-light text-sm'>Frontend Engineer</span>
+      </div>
     </motion.div>
   );
 }
--- app/resume/page.tsx
+++ app/resume/page.tsx
@@ -15,7 +15,7 @@ export default async function ResumePage() {
   return (
     <div className='flex flex-col gap-6'>
       {/* Logo and menu */}
-      <div className='px-4 pt-6 md:w-[1024px] md:mx-auto'>
+      <div className='px-4 py-6 md:w-[1024px] md:mx-auto sticky top-0 bg-white'>
         <div className='flex justify-between flex-col sm:flex-row gap-4'>
           <Logo />
           <div className='max-w-[400px] font-light text-[10px] text-gray-300 px-2'>
@@ -376,7 +376,7 @@ export default async function ResumePage() {
         </div>
       </div> */}
       {/* Say bye */}
-      <div className='pb-12 md:max-w-[1024px] md:mx-auto'>
+      <div className='pb-32 md:max-w-[1024px] md:mx-auto'>
         <Reveal className='flex flex-col items-center gap-8'>
           <Image
             src='/resume/build-product.jpg'
