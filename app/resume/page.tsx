04f734fb63137fea4dc200fb3dcc89bb9fcc366f
Fang Jin <windmaomao@gmail.com>
2025-08-20 12:10:33 -0400

    fix conflicts between sticky and other z-index

--- app/resume/page.tsx
+++ app/resume/page.tsx
@@ -15,7 +15,7 @@ export default async function ResumePage() {
   return (
     <div className='flex flex-col gap-6'>
       {/* Logo and menu */}
-      <div className='sticky top-0 bg-white'>
+      <div className='z-[9999] sticky top-0 bg-white'>
         <div className='px-4 py-6 md:w-[1024px] md:mx-auto flex justify-between flex-col sm:flex-row gap-4'>
           <Logo />
           <div className='max-w-[400px] font-light text-[10px] text-gray-300 px-2'>
@@ -376,7 +376,7 @@ export default async function ResumePage() {
         </div>
       </div> */}
       {/* Say bye */}
-      <div className='pb-32 md:max-w-[1024px] md:mx-auto'>
+      <div className='pb-12 md:max-w-[1024px] md:mx-auto'>
         <Reveal className='flex flex-col items-center gap-8'>
           <Image
             src='/resume/build-product.jpg'
