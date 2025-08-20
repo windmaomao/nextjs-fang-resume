6e9f0c204b8fadcb998f5a7c0bf24844a81024f6
Fang Jin <windmaomao@gmail.com>
2025-08-20 15:59:01 -0400

    use spring to scroll progress animation

--- app/resume/progress.tsx
+++ app/resume/progress.tsx
@@ -1,22 +1,27 @@
 'use client';
 
-import { motion, useScroll } from 'motion/react';
+import { motion, useScroll, useSpring } from 'motion/react';
 
 export default function Progress() {
   const { scrollYProgress } = useScroll();
+  const scaleX = useSpring(scrollYProgress, {
+    stiffness: 100,
+    damping: 30,
+    restDelta: 0.001,
+  });
 
   return (
     <motion.div
-      className='bg-sky-800'
+      className='bg-sky-500'
       id='scroll-indicator'
       style={{
-        scaleX: scrollYProgress,
+        scaleX,
         position: 'fixed',
-        zIndex: 20,
+        zIndex: 10000,
         top: 0,
         left: 0,
         right: 0,
-        height: 5,
+        height: 3,
         originX: 0,
       }}
     />
